9ecfc41bca4100b18daff14134d00b9aa8ec5af7
Anton Pirker <anton@ignaz.at>
2017-12-07 14:18:15 +0100

    Added service worker to have fullscreen feeling on mobiles.

---
+++ ws.js
@@ -0,0 +1,53 @@
+// Use a cacheName for cache versioning
+var cacheName = 'v3:static';
+
+// During the installation phase, you'll usually want to cache static assets.
+self.addEventListener('install', function(e) {
+    // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './assets/logo.png',
+                './assets/note.png',
+                './assets/playlist1.jpg',
+                './assets/playlist2.jpg',
+                './assets/playlist3.jpg',
+                './assets/playlist4.jpg',
+                './assets/playlist5.jpg',
+                './assets/playlist6.jpg',
+                './assets/playlist7.jpg',
+                './assets/playlist8.jpg',
+                './assets/playlist9.jpg',
+                './assets/track1.webp',
+                './assets/track2.webp',
+                './assets/track3.webp',
+                './assets/track4.webp',
+                './assets/track5.webp',
+
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb',
+                './assets/animate.css',
+                'https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN',
+                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh',
+                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ'
+            ]).then(function() {
+                self.skipWaiting();
+            });
+        })
+    );
+});
+
+// when the browser fetches a URL…
+self.addEventListener('fetch', function(event) {
+    // … either respond with the cached object or go ahead and fetch the actual URL
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
