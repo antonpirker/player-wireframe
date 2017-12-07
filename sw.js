2f7ebf5630bcab901359593001248efdf3c45b0b
Anton Pirker <anton@ignaz.at>
2017-12-07 14:26:45 +0100

    Removed external files from service worker

--- sw.js
+++ sw.js
@@ -23,13 +23,7 @@ self.addEventListener('install', function(e) {
                 './assets/track2.webp',
                 './assets/track3.webp',
                 './assets/track4.webp',
-                './assets/track5.webp',
-
-                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb',
-                './assets/animate.css',
-                'https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN',
-                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh',
-                'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ'
+                './assets/track5.webp'
             ]).then(function() {
                 self.skipWaiting();
             });
